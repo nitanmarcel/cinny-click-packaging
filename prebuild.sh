f7da64c0ab5393f2e5eb297f43a9e2d8ca69aa77
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2022-06-24 13:54:28 +0300

    Build specific cinny versions instead of the main development branch

--- prebuild.sh
+++ prebuild.sh
@@ -7,13 +7,16 @@ export REPO_URL="https://github.com/cinnyapp/cinny"
 export REPO_BRANCH="dev"
 export APP_TARGET="dist"
 
+export REPO_VERSION="2.0.4"
+export CLICK_VERSION_PREFIX="notificationfix1"
+
 if [ -d "${ROOT}/${REPO_NAME}" ]; then
     echo "Cleaning up"
     rm -rf "${ROOT}/${REPO_NAME}" "${ROOT}/target"
 fi
 echo "Cloning source repo"
 
-git clone "${REPO_URL}" -b "${REPO_BRANCH}" "${ROOT}/${REPO_NAME}" --depth=1
+git clone "${REPO_URL}" -b "${REPO_BRANCH}" "${ROOT}/${REPO_NAME}" --depth=1 --branch="v${REPO_VERSION}"
 cd "${ROOT}/${REPO_NAME}"
 
 if [ -d "${ROOT}/patches" ]; then
@@ -33,4 +36,9 @@ echo "Run npm build"
 npm run build
 echo "Copying ${APP_TARGET}"
 cp -r "${APP_TARGET}" "${ROOT}/target"
+
+sed -i "s/@CLICK_VERSION@/$REPO_VERSION$CLICK_VERSION_PREFIX/g" "${ROOT}/manifest.json.in"
+
+cat "${ROOT}/manifest.json.in"
+
 echo "Done. Creating click package."
